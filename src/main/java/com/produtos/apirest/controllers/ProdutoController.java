44b4e9e753377549cccd4ae61466647aa1975d44
Mikael Tavares <mikael.tavares@unigranrio.br>
2021-05-13 22:01:38 -0300

    Endpoint para salvar um produto adicionado.

--- src/main/java/com/produtos/apirest/controllers/ProdutoController.java
+++ src/main/java/com/produtos/apirest/controllers/ProdutoController.java
@@ -4,8 +4,11 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,5 +29,15 @@ public class ProdutoController {
 	@GetMapping("/produto/{id}")
 	public Optional<Produto> listaProdutoPorId(@PathVariable(value = "id") Long id){
 		return produtoRepository.findById(id);
+	}
+	
+	@PostMapping("/produto")
+	public Produto salvaProduto(@RequestBody Produto produto) {
+		return produtoRepository.save(produto);
+	}
+	
+	@DeleteMapping("/produto")
+	public void deletaProduto(@RequestBody Produto produto) {
+		produtoRepository.delete(produto);
 	}
 }
